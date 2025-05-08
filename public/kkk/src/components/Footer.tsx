2982902f87fc9e0feef0fbf0f4266889ca8ee9c7
KASYARA <140477415+fataakasyara@users.noreply.github.com>
2025-05-08 20:18:48 +0700

    Delete public/kkk directory

--- public/kkk/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const Footer = () => {
-  return <footer className="relative py-8">
-      <div className="container mx-auto px-4">
-        
-      </div>
-    </footer>;
-};
-export default Footer;
